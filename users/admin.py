d06130eab695eafa4eddb0c314597efb7a560dc9
sreeramvin <sreeramvin@gmail.com>
2020-07-28 13:36:31 +0530

    admin changes

--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Users
 # Register your models here.
-admin.site.register(Users)
+
+class UserPageAdmin(admin.ModelAdmin):
+    search_fields=['name']
+    
+admin.site.register(Users,UserPageAdmin)
